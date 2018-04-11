d88f41ea460f62a03695989ba8a4a1bd4a91bf73
Pavan Kuppa <kppa.pavan@gmail.com>
2018-04-12 01:14:02 +0530

    Create Items3.java

---
+++ app/src/main/java/com/example/dhananjay/dailygoals/Items3.java
@@ -0,0 +1,16 @@
+package com.example.android.dailygoals;
+
+public class Items3 {
+    private int image3;
+    public Items3(int image3)
+    {
+        this.image3=image3;
+    }
+    public int getImage() {
+        return image3;
+    }
+
+    public void setImage(int image3) {
+        this.image3 = image3;
+    }
+}
