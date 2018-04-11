038f4e7be289316b8d8409ad7335eaed3137ac61
Pavan Kuppa <kppa.pavan@gmail.com>
2018-04-12 01:13:30 +0530

    Create Items2.java

---
+++ app/src/main/java/com/example/dhananjay/dailygoals/Items2.java
@@ -0,0 +1,16 @@
+package com.example.android.dailygoals;
+
+public class Items2 {
+    private int image2;
+    public Items2(int image2)
+    {
+        this.image2=image2;
+    }
+    public int getImage() {
+        return image2;
+    }
+
+    public void setImage(int image2) {
+        this.image2 = image2;
+    }
+}
