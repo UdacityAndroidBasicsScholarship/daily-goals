73651998c4959c3b9542f137e90cf25d842c120b
Pavan Kuppa <kppa.pavan@gmail.com>
2018-04-13 19:00:50 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/dhananjay/dailygoals/Items1.java
@@ -0,0 +1,20 @@
+package com.example.dhananjay.dailygoals;
+
+/**
+ * Created by kmurali on 13-04-2018.
+ */
+
+public class Items1 {
+    private String text;
+    public Items1(String text)
+    {
+        this.text=text;
+    }
+    public  String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
---
+++ app/src/main/java/com/example/dhananjay/dailygoals/Items2.java
@@ -0,0 +1,20 @@
+package com.example.dhananjay.dailygoals;
+
+/**
+ * Created by kmurali on 13-04-2018.
+ */
+
+public class Items2 {
+    private int image2;
+    public Items2(int image2)
+    {
+        this.image2=image2;
+    }
+    public int getImage() {
+        return image2;
+    }
+
+    public void setImage(int image2) {
+        this.image2 = image2;
+    }
+}
