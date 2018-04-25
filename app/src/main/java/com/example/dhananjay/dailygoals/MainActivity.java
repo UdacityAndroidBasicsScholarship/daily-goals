c5e564b6e696fbaf9e399b4bec26944091b87cd1
Pavan Kuppa <kppa.pavan@gmail.com>
2018-04-25 21:38:30 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/dhananjay/dailygoals/MainActivity.java
+++ app/src/main/java/com/example/dhananjay/dailygoals/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.content.Intent;
+import android.view.View;
 
 import com.stephentuso.welcome.WelcomeHelper;
 
